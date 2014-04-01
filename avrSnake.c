4b63cab297e85f6fdc91a7a9f2855cd7f1f7ea04
Jacob Causon <jc29g12@soton.ac.uk>
2014-04-01 16:58:12 +0100

    Tidy/Removed rotary encoder from code as not used

--- avrSnake.c
+++ avrSnake.c
@@ -11,10 +11,6 @@
    - Slowed down to make tasks observable over serial connection
    - Adapted to avr-libc conventions
 
-   Modified by Jacob Causon, Feb 2014
-   - Added Idle sleep
-   - Idle LED indicator added
-
 
    Copyright (c) 2012 UC Regents. All rights reserved.
 
@@ -61,19 +57,12 @@
 #include <avr/interrupt.h>
 #include <math.h>
 #include <util/delay.h>
+
 #include "debug.h"
-
 #include "lcdlib/lcd.h"
-
-#define BLINK_TIME 50
-
-//Sleep mode = idle
-#define SLEEP_MODE 0
 
 #define TRUE 1
 #define FALSE 0
-#define true 1
-#define false 0
 
 #define LED_ON PORTB |= _BV(PINB7)
 #define LED_OFF PORTB &= ~_BV(PINB7)
@@ -121,41 +110,34 @@
    RIOS version 1.2
 */
 typedef struct task {
-   unsigned char running;     // 1 indicates task is running
-   int state;                 // Current state of state machine
-   unsigned long period;      // Rate at which the task should tick
-   unsigned long elapsedTime; // Time since task's previous tick
-   int (*TickFct)(int);       // Function to call for task's tick
+   unsigned char running;       // 1 indicates task is running
+   unsigned long period;        // Rate at which the task should tick
+   unsigned long elapsedTime;   // Time since task's previous tick
+   void (*TickFct)(void);       // Function to call for task's tick
 } task;
 
-task tasks[3];
 
-const unsigned char tasksNum = 3;
+#define TASK_NUMBER 2
+task tasks[TASK_NUMBER];
+const unsigned char tasksNum = TASK_NUMBER;
+
 uint8_t activeProcesses = 0;
 
 const double tick_ms = 1.0;  /* Real time between ticks in ms */
 const unsigned long tasksPeriodGCD = 1;  // Timer tick rate
-const unsigned long rotor_time = 1; //Every 1 ms
-const unsigned long button_time = 40;
-const unsigned long draw_time = 150;
-const unsigned long period4 = 200;
-
-// Stores the value of the scroll wheel
-volatile int16_t scroll_delta = 0;
+const unsigned long button_time = 40; //50ms
+const unsigned long draw_time = 150; //150ms
 
 void init_LED();
-void blink();
 
-int rotor_task(int state);
-int button_task(int state);
-int draw_task(int state);
+void button_task(void);
+void draw_task(void);
 
-unsigned char runningTasks[3] = {255}; // Track running tasks, [0] always idleTask
+unsigned char runningTasks[TASK_NUMBER] = {255}; // Track running tasks, [0] always idleTask
 const unsigned long idleTask = 255; // 0 highest priority, 255 lowest
 unsigned char currentTask = 0; // Index of highest priority task in runningTasks
 
 uint8_t ddrc, portc;
-
 
 //Black
 #define BACKGROUND_COLOR 0x0000 
@@ -178,7 +160,7 @@ uint8_t numColors = 7;
 int8_t selectedColor = 0;
 uint16_t colors[7] = {WHITE, BLUE, GREEN, CYAN, RED, MAGENTA, YELLOW};
 
-char coverOld = true;
+char coverOld = TRUE;
 //rectangle rectOld = {10,20,10,20};
 rectangle rect = {10,20,10,20};
 
@@ -211,24 +193,16 @@ ISR(TIMER1_COMPA_vect) {
          currentTask += 1;
          runningTasks[currentTask] = i; // Add to runningTasks
          activeProcesses++; //Count the number of active processes
-        // LED_ON;
        sei();
 	 
-         tasks[i].state = tasks[i].TickFct(tasks[i].state); // Execute tick
+         tasks[i].TickFct(); // Execute tick
          
        cli();	 
          tasks[i].running = 0; // Mark as not running
          runningTasks[currentTask] = idleTask; // Remove from runningTasks
          currentTask -= 1;
          activeProcesses--; //Count the number of active processes
-         if(activeProcesses == 0){ 
-        //    LED_OFF;
-         //   sleep_enable();
-            sei();
-          //  sleep_cpu();       
-         } else {
-            sei();
-         }
+       sei();
       }
       tasks[i].elapsedTime += tasksPeriodGCD;
    }
@@ -264,11 +238,6 @@ void init_LED() {
     
 }
 
-void init_sleep()
-{
-    set_sleep_mode(SLEEP_MODE);
-}
-
  void init_stdio_uart1(void)
  {
 	 /* Configure UART1 baud rate, one start bit, 8-bit, no parity and one stop bit */
@@ -302,111 +271,40 @@ void init_snake()
     snakeHead = 2;
     gameOver = FALSE;
 
-    char buttonPressed = 'E';
-    Point snake[SNAKE_LENGTH];
-    Point food = {5,5};
+    buttonPressed = 'E';
+    food.x = 5;
+    food.y = 5;
 
     clear_screen();
 }
 
 int main(void)
 {
+    init_debug_uart1(); //Can be removed when not debugging
+    init_LED();
 
-   init_debug_uart1();
-
-   
-   init_LED();
-//     init_stdio_uart1();
-    
-
-   init_lcd();
+    init_lcd();
     portc  = PORTC;  /* Store display configuration of Port C */
-     ddrc   = DDRC;
-
-   // Priority assigned to lower position tasks in array
-   unsigned char i = 0;
-
-   tasks[i].state = -1;
-   tasks[i].period = rotor_time;
-   tasks[i].elapsedTime = tasks[i].period;
-   tasks[i].running = 0;
-   tasks[i].TickFct = &rotor_task;
-   ++i;
-   tasks[i].state = -1;
-   tasks[i].period = button_time;
-   tasks[i].elapsedTime = tasks[i].period;
-   tasks[i].running = 0;
-   tasks[i].TickFct = &button_task;
-   ++i;
-   tasks[i].state = -1;
-   tasks[i].period = draw_time;
-   tasks[i].elapsedTime = tasks[i].period;
-   tasks[i].running = 0;
-   tasks[i].TickFct = &draw_task;
-
-   init_processor();
-
+    ddrc   = DDRC;
     
-   init_snake();
-    
-   while(1);   
-}
+    init_snake();
 
-int rotor_task(int state) {
-    cli();
-    static int8_t last;
-    uint8_t ddrc, portc;
-    uint8_t ddrd, portd;
-    int8_t new, diff;
-    uint8_t wheel;
+    // Priority assigned to lower position tasks in array
+    unsigned char i = 0;
 
-    /* Save state of Port C & D */
-    ddrc   = DDRC; portc  = PORTC;
-    ddrd   = DDRD; portd  = PORTD;
+    tasks[i].period = button_time;
+    tasks[i].elapsedTime = tasks[i].period;
+    tasks[i].running = 0;
+    tasks[i].TickFct = &button_task;
+    ++i;
+    tasks[i].period = draw_time;
+    tasks[i].elapsedTime = tasks[i].period;
+    tasks[i].running = 0;
+    tasks[i].TickFct = &draw_task;
 
-    CZ C2_H C3_H C2_R C3_R
-    D0_Z D1_L
-    _delay_us(3);
-    wheel = PINC;
-
-    /*
-    Scan rotary encoder
-    ===================
-    This is adapted from Peter Dannegger's code available at:
-    http://www.mikrocontroller.net/attachment/40597/ENCODE.C
-    */
-
-    new = 0;
-    if( wheel  & _BV(PC3) ) {
-        new = 3;
-    }
-    if( wheel  & _BV(PC2) ) {
-        new ^= 1;			        // convert gray to binary
-    }
-    diff = last - new;			// difference last - new
-    if( diff & 1 ){			// bit 0 = value (1)
-        last = new;		       	// store new as next last
-        scroll_delta += ((diff & 2) - 1);	// bit 1 = direction (+/-)
-    }
-
-    /* Restore state of Port C */
-    DDRC    = ddrc; PORTC   = portc;
-    DDRD    = ddrd; PORTD   = portd;
-
-    PORTA &= ~_BV(PA0);  /* ISR Timing End */
-    sei();
-    return ++state;
-}
-
-int16_t iob_delta(){
-    int16_t val;
-
-    cli();
-    val = scroll_delta;
-    scroll_delta &= 1;
-    sei();
-
-    return val >> 1;
+    init_processor();
+ 
+    while(1);   
 }
 
 void pushed(char type) {
@@ -429,68 +327,50 @@ void pushed(char type) {
                 buttonPressed = 'E';
            break; 
         case 'C':
-           coverOld = false;
+           coverOld = FALSE;
            break;
     }
 }
 
-int button_task(int state) {
+void button_task(void) {
     //printf( "-> Rotor [% 3d]\n", scroll_delta);
-D0_H
-         D0_R
+    D0_H
+     D0_R
 
-         C2_H C3_H C4_H C5_L
-         _delay_ms(3);
-         if (!(PIND & _BV(PD0))) { pushed('S'); }
+     C2_H C3_H C4_H C5_L
+     _delay_ms(3);
+     if (!(PIND & _BV(PD0))) { pushed('S'); }
 
-             C2_H C3_H C4_L C5_H
-         _delay_ms(3);
-         if (!(PIND & _BV(PD0))) { pushed('W'); }
+     C2_H C3_H C4_L C5_H
+     _delay_ms(3);
+     if (!(PIND & _BV(PD0))) { pushed('W'); }
 
 
-             C2_H C3_L C4_H C5_H
-         _delay_ms(3);
-         if (!(PIND & _BV(PD0))) { pushed('N'); }
+     C2_H C3_L C4_H C5_H
+     _delay_ms(3);
+     if (!(PIND & _BV(PD0))) { pushed('N'); }
 
-             C2_L C3_H C4_H C5_H
-         _delay_ms(3);
-         if (!(PIND & _BV(PD0))) { pushed('E'); }
+         C2_L C3_H C4_H C5_H
+     _delay_ms(3);
+     if (!(PIND & _BV(PD0))) { pushed('E'); }
 
-         D0_Z
+     D0_Z
 
-         D1_H 
-         D1_R
+     D1_H 
+     D1_R
          
-        if(gameOver) {
-                 C2_Z C3_Z C4_L C5_Z
-             if (!(PIND & _BV(PD1))) { init_snake(); }
+    if(gameOver) {
+         C2_Z C3_Z C4_L C5_Z
+         if (!(PIND & _BV(PD1))) { 
+            LED_OFF;
+            init_snake(); 
         }
+    }
 
-       //  D1_L
+    DDRC    = ddrc;  /* Restore display configuration of Port C */
+    PORTC   = portc;
 
-       //      C2_R C3_R
-       /*  if (!(PINC & _BV(PC2))) { pushed("A"); }
-
-
-         if (!(PINC & _BV(PC3))) { pushed("B "); }*/
-
-
-         DDRC    = ddrc;  /* Restore display configuration of Port C */
-         PORTC   = portc;
-
-        int16_t d = iob_delta();
-        selectedColor += d;//scroll_delta;
-        scroll_delta = 0;
-        if(selectedColor >= numColors) {
-            selectedColor = 0;
-        } else if(selectedColor < 0) {
-            selectedColor = numColors -1;
-        }
-        //selectedColor = selectedColor % numColors;
-        if(d != 0)
-            printf("new color %d delta %d\n", selectedColor, d);
-
-    return ++state;
+    return;
 }
 
 void updateSnake()
@@ -535,7 +415,7 @@ void updateSnake()
     }
 }
 
-int draw_task(int state) {
+void draw_task(void) {
     DDRC    = ddrc;  // Restore display configuration of Port C
     PORTC   = portc;
     _delay_ms(3);
@@ -543,7 +423,8 @@ int draw_task(int state) {
 
     if(gameOver) {
         display_string("GAME OVER \n");
-        return 0;
+        LED_ON;
+        return;
     }
 
     uint8_t h = snakeHead;
@@ -601,12 +482,12 @@ int draw_task(int state) {
     rect.left = 0;
     draw_rectangle(rect, BLUE);
 
-    coverOld = true;
+    coverOld = TRUE;
     D1_Z
 
     D0_H
     D0_R
 
-    return ++state;
+    return;
 }
 
